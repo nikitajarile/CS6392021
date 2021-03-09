f7d65af29a1b6647e411d3711020745caa6bab09
nikitajarile <nj57824n@pace.edu>
2021-03-08 19:31:08 -0500

    Added PowerReceiver App

---
+++ PowerReceiver/app/src/main/java/com/example/powerreceiver/CustomReceiver.java
@@ -0,0 +1,37 @@
+package com.example.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    private static final String ACTION_CUSTOM_BROADCAST = BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+
+        if(intentAction != null){
+            String  toastMessage = "Unknown Intent action";
+
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED :
+                    toastMessage = "Power Connected!";
+                    break;
+
+                case Intent.ACTION_POWER_DISCONNECTED :
+                    toastMessage = "Power Disconnected!";
+                    break;
+
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = "Custom Broadcast Received";
+                    break;
+            }
+
+            Toast.makeText(context,toastMessage,Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
